63cb27a79edbb544b8fe40cfd6aacf1022f1f734
Michael Ren <37400755+micha31r@users.noreply.github.com>
2025-08-17 00:17:50 +1000

    Improve app frame for overflow

--- components/app-frame.tsx
+++ components/app-frame.tsx
@@ -2,8 +2,10 @@ import React from "react";
 
 export function AppFrame({ children }: { children?: React.ReactNode }) {
   return (
-    <main className="flex relative h-[100svh] w-full max-w-96 bg-background rounded-[48px] px-4 py-8">
-      {children}
+    <main className="flex flex-col h-[100svh] w-full max-w-96 bg-background rounded-[48px] px-4 pt-8 overflow-y-auto">
+      <div className="flex flex-col flex-1 pb-8">
+        {children}
+      </div>
     </main>
   )
 }
