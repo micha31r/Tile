d3527fada4cd927028f1c0127c090e47d2e7ede9
Michael Ren <37400755+micha31r@users.noreply.github.com>
2025-08-17 01:33:51 +1000

    Create app home page

---
+++ app/app/page.tsx
@@ -0,0 +1,17 @@
+import { redirect } from "next/navigation";
+import { createClient } from "@/lib/supabase/server";
+
+export default async function AppHomePage() {
+  const supabase = await createClient();
+
+  const { data, error } = await supabase.auth.getClaims();
+  if (error || !data?.claims) {
+    redirect("/auth/login");
+  }
+
+  return (
+    <div className="flex-1 w-full flex flex-col gap-12">
+      
+    </div>
+  );
+}
