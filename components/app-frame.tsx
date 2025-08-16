4772b9e29a00139c275a2dd6e0e167ba1bdc03db
Michael Ren <37400755+micha31r@users.noreply.github.com>
2025-08-17 00:10:27 +1000

    Improve app frame

--- components/app-frame.tsx
+++ components/app-frame.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export function AppFrame({ children }: { children?: React.ReactNode }) {
   return (
-    <main className="relative h-[100svh] w-full max-w-96 bg-background rounded-[48px] px-4 py-8">
+    <main className="flex relative h-[100svh] w-full max-w-96 bg-background rounded-[48px] px-4 py-8">
       {children}
     </main>
   )
