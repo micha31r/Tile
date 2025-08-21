053188eeb3e43e8153a6f26f59c59baf4724973b
Michael Ren <37400755+micha31r@users.noreply.github.com>
2025-08-22 06:27:58 +1000

    Update app-frame.tsx

--- components/app-frame.tsx
+++ components/app-frame.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export function AppFrame({ children }: { children?: React.ReactNode }) {
   return (
     <main className="flex flex-col h-[100svh] w-full sm:max-w-96 max-w-md bg-background sm:rounded-[48px] px-4 sm:pt-8 pt-4 overflow-y-auto">
-      <div className="flex flex-col flex-1 sm:pb-8 pb-4">
+      <div className="flex flex-col flex-1 pb-8">
         {children}
       </div>
     </main>
