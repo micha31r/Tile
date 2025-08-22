f24857d40318e4789118d4a8331c5f27b864d6e5
Michael Ren <37400755+micha31r@users.noreply.github.com>
2025-08-22 20:52:33 +1000

    Update logout-button.tsx

--- components/logout-button.tsx
+++ components/logout-button.tsx
@@ -1,18 +1,16 @@
 "use client";
 
 import { createClient } from "@/lib/supabase/client";
-import { useRouter } from "next/navigation";
 import { useHaptic } from "react-haptic";
 
 export function LogoutButton({ className }: { className?: string }) {
-  const router = useRouter();
   const { vibrate } = useHaptic();
 
   const logout = async () => {
     vibrate();
     const supabase = createClient();
     await supabase.auth.signOut();
-    router.push("/auth/login");
+    window.location.href = "/auth/login";
   };
 
   return <button onClick={logout} className={className}>Logout</button>;
