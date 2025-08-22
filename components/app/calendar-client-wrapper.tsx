c0838a7947efcfee7510f7c6c4e07b74ebb269b5
Michael Ren <37400755+micha31r@users.noreply.github.com>
2025-08-22 20:28:19 +1000

    Update calendar-client-wrapper.tsx

--- components/app/calendar-client-wrapper.tsx
+++ components/app/calendar-client-wrapper.tsx
@@ -2,17 +2,19 @@
 
 import Link from "next/link";
 import { CalendarMonth, Month } from "./calendar-month";
+import { useHaptic } from "react-haptic";
 
 export function CalendarClientWrapper({ userId }: { userId: string }) {
   const now = new Date();
   const year = now.getFullYear();
+  const { vibrate } = useHaptic();
 
   return (
     <div className="space-y-4">
       <div className="flex items-center justify-between">
         <h3 className="font-medium">{now.toLocaleString('default', { month: 'long' })}</h3>
         <div>
-          <Link href={`/app/calendar`}>
+          <Link href={`/app/calendar`} onClick={vibrate}>
             <button className="hover:scale-95 transition-transform flex items-center justify-center gap-1 rounded-full w-max h-8 p-2 px-3 bg-secondary text-sm font-medium text-muted-foreground">
               More
             </button>
