adb99d145ea89e3542c8bafb6c8bf567a4b1a25b
Michael Ren <37400755+micha31r@users.noreply.github.com>
2025-08-23 16:20:16 +1000

    Increase logo size for loading screen

--- app/loading.tsx
+++ app/loading.tsx
@@ -4,7 +4,7 @@ import { cn } from "@/lib/utils";
 export default function Loading({ className }: { className?: string }) {
   return (
     <div className={cn("flex items-center justify-center h-full", className)}>
-      <Logo />
+      <Logo tileWidth={28} />
     </div>
   );
 }
